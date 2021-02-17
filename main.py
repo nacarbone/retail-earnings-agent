92d4524ad7f85ed5a9314aa0f58c92f42a847e5e
Nicholas Carbone <carboneni@gmail.com>
2021-02-16 18:19:43 -0800

    Update analysis for homework 4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     ray.init(ignore_reinit_error=True)
     training_helper = TrainingHelper(
         custom_ppo_config={'num_gpus' : 1},
-        custom_train_config={'restore' : True,
-                            'chkpt_root' : 'ray_results/2021-02-16_13-20-56'}
-    )
-    training_helper.train(1)
+#         custom_train_config={'restore' : True,
+#                             'chkpt_root' : 'ray_results/2021-02-16_13-20-56'}
+     )
+    training_helper.train(500)
